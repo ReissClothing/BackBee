9519d16118ec59b7db852a0c53e67c9557b69ca0
Gonzalo Vilaseca <gonzalo.vilaseca@reiss.com>
2015-12-16 17:18:24 +0000

    fix

--- src/BackBee/CoreDomain/NestedNode/AbstractNestedNode.php
+++ src/BackBee/CoreDomain/NestedNode/AbstractNestedNode.php
@@ -23,7 +23,7 @@
 
 namespace BackBee\CoreDomain\NestedNode;
 
-use BackBee\CoreDomainBundle\Utils\Numeric;
+use BackBee\CoreDomain\Utils\Numeric;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
