1f282a39354305134e4e0a55b3bacf01fcdb6fb0
Gonzalo Vilaseca <gonzalo.vilaseca@reiss.com>
2015-12-16 17:16:18 +0000

    fix

--- src/BackBee/CoreDomain/Utils/Numeric.php
+++ src/BackBee/CoreDomain/Utils/Numeric.php
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with BackBee. If not, see <http://www.gnu.org/licenses/>.
  */
-namespace BackBee\CoreDomainBundle\Utils;
+namespace BackBee\CoreDomain\Utils;
 
 /**
  * Set of utility methods to deal with numeric varaibles
